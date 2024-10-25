649cdbfef054bdb18d6e6cb6c94e36b5962763fa
Lisofffa <lisofffa@gmail.com>
2024-10-25 12:29:20 +0300

    fix: update buildings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,38 @@
 import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-// import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
-
+import BrowserOnly from "@docusaurus/BrowserOnly"; // import BrowserOnly
 import styles from "./index.module.css";
+import { useEffect } from "react";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
-
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+    <BrowserOnly>
+      {() => {
+        useEffect(() => {
+          if (window) {
+            window.location.href = "docs/";
+          }
+        }, []);
 
-          <meta charSet="UTF-8" />
-          <meta http-equiv="refresh" content="0; url=docs/" />
-          <script type="text/javascript">
-            {(window.location.href = "docs/")}
-          </script>
-          <title>Your Site Title Here</title>
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/introduction-to-mutable-web/"
-          >
-            {/* todo: need correct */}
-            Dap Inro- 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
+        return (
+          <header className={clsx("hero hero--primary", styles.heroBanner)}>
+            <div className="container">
+              <Heading as="h1" className="hero__title">
+                {siteConfig.title}
+                <meta charSet="UTF-8" />
+                <meta http-equiv="refresh" content="0; url=docs/" />
+                <title>Your Site Title Here</title>
+              </Heading>
+              <p className="hero__subtitle">{siteConfig.tagline}</p>
+              <div className={styles.buttons}></div>
+            </div>
+          </header>
+        );
+      }}
+    </BrowserOnly>
   );
 }
 
@@ -44,7 +41,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout title={siteConfig.title}>
       <HomepageHeader />
-      <main>{/* <HomepageFeatures /> */}</main>
+      <main></main>
     </Layout>
   );
 }
